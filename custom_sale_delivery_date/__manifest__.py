38aec94fe943095cbf5d1042cdcc783279f96714
RMorales202010033 <jrodolfomc2002@gmail.com>
2024-10-07 15:59:16 -0600

    Custom sale delivery date v2

--- custom_sale_delivery_date/__manifest__.py
+++ custom_sale_delivery_date/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Custom Sale Delivery Date',
+    'name': 'Custom Sale Delivery Date v2',
     'version': '1.0',
     'summary': 'Modificación para ajustar la fecha de entrega en ventas',
     'description': 'Ajusta automáticamente la fecha de entrega a 5 días después de la confirmación del pedido',
