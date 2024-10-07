d5a8deaa7e5e3317eea728b30c21155abd75d805
RMorales202010033 <jrodolfomc2002@gmail.com>
2024-10-07 14:46:37 -0600

    Delivery date test

---
+++ custom_sale_delivery_date/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Sale Delivery Date',
+    'version': '1.0',
+    'summary': 'Modificación para ajustar la fecha de entrega en ventas',
+    'description': 'Ajusta automáticamente la fecha de entrega a 5 días después de la confirmación del pedido',
+    'category': 'Sales',
+    'author': 'Rodolfo Morales',
+    'depends': ['sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_sale_delivery_date/models/sale_order.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+from datetime import timedelta
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        date_action = fields.datetime('Date current action', default=fields.Datetime.now, required=False, readonly=False, select=True)
+        # delivery_date = fields.Datetime.now() + timedelta(days=5)
+        delivery_date = date_action
+
+        self.write({
+            'commitment_date': delivery_date
+        })
+
+        return res
