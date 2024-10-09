d4cf17f8473cb3d94e2c074c2ea68d332f17b3a2
RMorales202010033 <jrodolfomc2002@gmail.com>
2024-10-09 15:07:41 -0600

    CSDD 2.3

--- custom_sale_delivery_date/__manifest__.py
+++ custom_sale_delivery_date/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    'name': 'Custom Sale Delivery Date v2.1',
-    'version': '1.1',
+    'name': 'Custom Sale Delivery Date v2.3',
+    'version': '2',
     'summary': 'Modificación para ajustar la fecha de entrega en ventas',
     'description': 'Ajusta automáticamente la fecha de entrega a 5 días después de la confirmación del pedido',
     'category': 'Sales',
