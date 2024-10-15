5d1a0cacf0d1168979df135ca6ff596fa097aac9
RMorales202010033 <jrodolfomc2002@gmail.com>
2024-10-15 09:26:16 -0600

    Custom sale delivery date module v1

---
+++ custom_sale_delivery_date/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Sale Delivery Date v2.1',
+    'version': '2',
+    'summary': 'Modificación para ajustar la fecha de entrega en ventas',
+    'description': 'Ajusta automáticamente la fecha de entrega a 5 días después de la confirmación del pedido',
+    'category': 'Sales',
+    'author': 'Rodolfo Morales',
+    'depends': ['sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_sale_delivery_date/models/__init__.py
@@ -0,0 +1,1 @@
+from . import sale_order_delivery_date
